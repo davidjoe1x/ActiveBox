5efca9e37b639b7cdab60eb5ca6116946dffaaf7
davidjoe1x <david.zograbian@mail.ru>
2022-10-20 18:04:17 +0300

    activebox desktop

---
+++ js/swiper-init.js
@@ -0,0 +1,37 @@
+const swiper = new Swiper('.swiper', {
+    // Optional parameters
+    direction: 'horizontal',
+    loop: false,
+
+  
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+      clickable:true,
+      dynamicBullets: true,
+      dynamicMainBullets: 5,
+      autoplay: {
+        delay: 3000,
+      },
+    },
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  
+    // And if we need scrollbar
+    scrollbar: {
+      el: '.swiper-scrollbar',
+    },
+
+    autoplay: {
+      delay: 3000,
+    },
+
+
+
+
+
+  });
